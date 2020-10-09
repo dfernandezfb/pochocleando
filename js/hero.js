391c428a32609b7a675e8b2da54ce608cfc2f4a6
RominaEstrada <romiiestrada@gmail.com>
2020-10-09 14:57:11 -0300

    Task1-09

---
+++ js/hero.js
@@ -0,0 +1,47 @@
+const containerMain = document.querySelector('#container-main');
+const URLseries = 'http://localhost:3000/series';
+const URLpeliculas = 'http://localhost:3000/peliculas';
+
+async function getSeries() {
+    const response = await fetch(URLseries);
+    const data = await response.json();
+    return data;
+}
+async function getPeliculas() {
+    const response = await fetch(URLpeliculas);
+    const data = await response.json();
+    return data;
+}
+async function buildSlides() {
+    let series = await getSeries();
+    let peliculas = await getPeliculas();
+    const datos = series.concat(peliculas);
+    console.log(datos)
+    for (let i = 0; i < datos.length; i++) {
+        if (datos[i].destacada === true) {
+            containerSlider.innerHTML += `                 
+            <div class="carousel-item hero ${i === 0 ? 'active' : ''}" >
+             <img src="${datos[i].imagen}" class="d-block w-100 img-hero " alt="img-serie">
+              <div class="carousel-caption d-none d-md-block ">
+                    <h5 class="text mt-2 title-black title">${datos[i].nombre}</h5>
+                    <p class="user-select-none p-black text-center">${datos[i].descripcion}</p>
+                    <a href="error404.html"> <button class="btn-hero btn-white bi bi-play mt-n3" id="btn-slide">
+                     <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-play-fill" fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                     </svg> REPRODUCIR </button>
+                    </a>
+              </div>              
+            </div> 
+             `
+            carouselIndicators.innerHTML += `
+             <li data-target="#container-main" data-slide-to="${i}" class="${i === 0 ? 'active' : ''}"></li>
+              `
+        }
+    }
+}
+const containerSlider = document.getElementById('inner-carousel');
+const carouselIndicators = document.querySelector('.carousel-indicators');
+
+buildSlides();
+
