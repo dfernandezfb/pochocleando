c75151bcc1ccd6f8d92aa480054bd5736389665a
Diego Fernandez <dfernandezfb@gmail.com>
2020-10-10 02:18:28 -0300

    agrego funcionalidad con servidor y con local storage, decidir cual usar

--- js/categories.js
+++ js/categories.js
@@ -35,13 +35,33 @@ async function getMovies() {
     const data = await response.json();
     return data;
 }
+async function getMovie(id) {
+    const newURL= `${URL}/${id}`
+    const response = await fetch(newURL);
+    const data = await response.json();
+    return data;
+}
+
+async function editMovie(id,newData){
+    const newURL = `${URL}/${id}`;
+    const response = await fetch(newURL,{
+        method:'PUT',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(newData)
+    });
+    const data = await response.json();
+    return data;
+}
 const com = document.querySelector('.carrousel-Comedia');
 const ter = document.querySelector('.carrousel-Terror');
 const dra = document.querySelector('.carrousel-Drama');
 const acc = document.querySelector('.carrousel-Acción');
 const cie = document.querySelector('.carrousel-Ficción');
 
-function buildComedy(movies){
+function buildMovieCards(movies){
     movies.map(movie => {
         if(movie.publicada==='true')
         {
@@ -148,63 +168,85 @@ function buildComedy(movies){
     })
 }
 
-getMovies()
-.then(movies => buildComedy(movies));
-
-function getMoviesLS()
+function buildFavsList(movies)
 {
-    let movies;
-    if(localStorage.getItem('movies')===null)
+    movies.map(movie=>
     {
-        movies=[]
-    }else{
-        movies=JSON.parse(localStorage.getItem('movies'));
-    }
-    return movies;
-}
-
-function saveMoviesLS(movieInfo)
-{
-    let movies = getMoviesLS();
-    movies.push(movieInfo);
-    localStorage.setItem('movies', JSON.stringify(movies));
-}
-function addMovieToFavs(productInfo)
-{
-    const favorite=document.createElement("tr")
-    favorite.innerHTML=`
-        <td class="img-fav">
-            ${productInfo.imagen}
-        </td>
-        <td class="name-fav">
-            ${productInfo.nombre}
-        </td>
-        <td class="img-fav">
-            <span>&times</span>
-        </td>
-    `;
-    console.log("hola");
-    favoritesList.appendChild(favorite);
-}
-function rendermoviesLS()
-{
-    let movies = getMoviesLS();
-    movies.forEach((movie)=>{
-    const favorite=document.createElement("tr")
-    favorite.innerHTML=`
-        <td class="img-fav">
-            ${movie.imagen}
-        </td>
-        <td class="name-fav">
+        if (movie.fav===true)
+        {
+            const favorite=document.createElement("tr");
+            favorite.id=`${movie.id}`;
+            favorite.innerHTML=`
+           
+            <img src="${movie.imagen}" alt="hola">
+            <td class="name-fav">
             ${movie.nombre}
-        </td>
-        <td class="img-fav">
-            <span>&times</span>
-        </td>
-    `;
-    favoritesList.appendChild(favorite);
+            </td>
+            <td class="close-fav">
+            <a href=# class="delete-fav">&times</a>
+            </td>
+            `;
+            favoritesList.appendChild(favorite);
+        }
     })
 }
+
+function modifyFav(e)
+{
+    e.preventDefault();
+    if(e.target.classList.contains("fav-btn"))
+    {    
+        let id;
+        let movieInfo=e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
+        if(movieInfo.classList.contains("movie"))
+        {
+            id= movieInfo.id;
+        }
+        else
+        {
+            movieInfo=movieInfo.querySelector(".movie");
+            id= movieInfo.id;
+        }
+        getMovie(id)
+        .then(movie=>modifyFavServer(movie))
+    }
+}
+
+function modifyFavServer(movie)
+{
+    movie.fav=(movie.fav===true)? false : true;
+    const newData={
+        id:movie.id,
+        nombre:movie.nombre,
+        descripcion:movie.descripcion,
+        director:movie.director,
+        genero:movie.genero,
+        categoria:movie.categoria,
+        publicada:movie.publicada,
+        año:movie.año,
+        imagen:movie.imagen,
+        video:movie.video,
+        fav:movie.fav
+    };
+    editMovie(movie.id,newData);
+}
+
+function deleteFav(e)
+{
+    if(e.target.classList.contains("delete-fav"))
+    e.preventDefault();
+    {
+        let id = e.target.parentElement.parentElement.id;
+        getMovie(id)
+        .then(movie=>modifyFavServer(movie))
+    }
+}
+
+getMovies()
+.then(movies => buildMovieCards(movies))
+getMovies()
+.then(movies => buildFavsList(movies))
+
 containerCategories.addEventListener("click",(event)=>{
     if(event.target.classList.contains("angle-right"))
     {   
@@ -247,33 +289,129 @@ containerCategories.addEventListener("click",(event)=>{
         }
     }
 })
-document.addEventListener("DOMContentLoaded",rendermoviesLS);
-containerCategories.addEventListener("click",(e)=>{
-    e.preventDefault();
-    if(e.target.classList.contains("fav-btn"))
-    {
-        let movieInfo=e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
-        if(movieInfo.classList.contains("movie"))
-        {
-            movieInfo=
-            {
-                imagen: movieInfo.style.backgroundImage.slice(5,-2),
-                nombre: movieInfo.querySelector(".movie-title").textContent,
-                id: movieInfo.id
-            }
-        }
-        else
-        {
-            movieInfo=movieInfo.querySelector(".movie");
-            movieInfo=
-            {
-                imagen: movieInfo.style.backgroundImage.slice(5,-2),
-                nombre: movieInfo.querySelector(".movie-title").textContent,
-                id: movieInfo.id
-            }
-        }
-        addMovieToFavs(movieInfo)
-        saveMoviesLS(movieInfo)
-    }
-})
 
+containerCategories.addEventListener("click",modifyFav)
+favoritesList.addEventListener("click",deleteFav)
+
+
+
+
+
+
+
+
+
+//----------------------FAVORITOS CON LOCAL STORAGE--------------------
+
+// function getMoviesLS()
+// {
+//     let movies;
+//     if(localStorage.getItem('movies')===null)
+//     {
+//         movies=[]
+//     }else{
+//         movies=JSON.parse(localStorage.getItem('movies'));
+//     }
+//     return movies;
+// }
+
+// function saveMoviesLS(movieInfo)
+// {
+//     let movies = getMoviesLS();
+//     movies.push(movieInfo);
+//     localStorage.setItem('movies', JSON.stringify(movies));
+// }
+// function addMovieToFavs(productInfo)
+// {
+//     const favorite=document.createElement("tr")
+//     favorite.id=`${productInfo.id}`;
+//     favorite.innerHTML=`
+//         <td class="img-fav">
+//             ${productInfo.imagen}
+//         </td>
+//         <td class="name-fav">
+//             ${productInfo.nombre}
+//         </td>
+//         <td class="close-fav">
+//             <a href=# class="delete-fav">&times</a>
+//         </td>
+//     `;
+//     favoritesList.appendChild(favorite);
+// }
+// function rendermoviesLS()
+// {
+//     let movies = getMoviesLS();
+//     movies.forEach((movie)=>{
+//     const favorite=document.createElement("tr")
+//     favorite.id=`${movie.id}`;
+//     favorite.innerHTML=`
+//         <td class="img-fav">
+//             ${movie.imagen}
+//         </td>
+//         <td class="name-fav">
+//             ${movie.nombre}
+//         </td>
+//         <td class="close-fav">
+//             <a href=# class="delete-fav">&times</a>
+//         </td>
+//     `;
+//     favoritesList.appendChild(favorite);
+//     })
+// }
+// function deleteFav(e)
+// {
+//     e.preventDefault();
+//     if(e.target.classList.contains("delete-fav"))
+//     {
+//         const removedElement = e.target.parentElement.parentElement
+//         const deleteId= removedElement.id;
+//         e.target.parentElement.parentElement.remove();
+//         console.log(deleteId);
+//         deleteFavLS(deleteId);
+//     }
+// }
+// function deleteFavLS(deleteId)
+// {
+//     console.log("hola");
+//     let movies = getMoviesLS();
+//     movies.forEach((movie,index)=>{
+//         if(movie.id === deleteId)
+//         {
+//             console.log("hola");
+//             movies.splice(index,1);
+//         }
+//     })
+//     localStorage.setItem("movies",JSON.stringify(movies));
+// }
+// document.addEventListener("DOMContentLoaded",rendermoviesLS);
+// containerCategories.addEventListener("click",(e)=>{
+//     e.preventDefault();
+//     if(e.target.classList.contains("fav-btn"))
+//     {
+//         let movieInfo=e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
+//         if(movieInfo.classList.contains("movie"))
+//         {
+//             e.target.classList.add("color3")
+//             movieInfo=
+//             {
+//                 imagen: movieInfo.style.backgroundImage.slice(5,-2),
+//                 nombre: movieInfo.querySelector(".movie-title").textContent,
+//                 id: movieInfo.id
+//             }
+//         }
+//         else
+//         {
+//             e.target.children.classList.add("color3")
+//             movieInfo=movieInfo.querySelector(".movie");
+//             movieInfo=
+//             {
+//                 imagen: movieInfo.style.backgroundImage.slice(5,-2),
+//                 nombre: movieInfo.querySelector(".movie-title").textContent,
+//                 id: movieInfo.id
+//             }
+//         }
+//         addMovieToFavs(movieInfo)
+//         saveMoviesLS(movieInfo)
+//     }
+// })
+// document.addEventListener("click",deleteFav);
