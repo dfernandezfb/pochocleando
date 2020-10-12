96e79721404eb72a36e57edb928593be29a1e4e9
RominaEstrada <romiiestrada@gmail.com>
2020-10-12 01:55:46 -0300

    Se agregó funciones a login.js

--- js/login.js
+++ js/login.js
@@ -1,69 +1,40 @@
 'use strict'
-const user = document.querySelector('#user');
-const links = document.querySelector('#links')
+const renderUser = document.getElementById('user');
+//renderUser.classList.add('d-flex');
+const links = document.querySelector('#links');
 const login = document.querySelector('#login');
 const registro = document.querySelector('#registro');
 
-
-
-function readUser(user) {
-    user = {
-        email: user.querySelector('#email').value,
-        password: user.querySelector('#password').value,
-    }
-    if (!(user.email === '' && user.password === '')) {
-        saveUserOnLS(user);
-    }
-}
-function saveUserOnLS(element) {
-    let user;
-    user = getUsersFromLS();
-    user.push(element);
-    localStorage.setItem('users', JSON.stringify(user));
+let user = {
+    nombre: "Romina",
+    apellido: "Estrada",
+    admin: true,
 }
 
-function getUsersFromLS() {
-    let users;
-    if (localStorage.getItem('users') === null) {
-        users = [];
+saveUserOnLS(user);
+showUser();
+
+function saveUserOnLS(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+}
+function showUser() {
+    removeLinksFromDOM();
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (user.admin === true) {
+        renderUser.innerHTML = `<div class="d-flex align-content-end flex-wrap">
+        <h5 class="p-white"><i class="fas fa-user-cog"></i>¡Bienvenido ${user.nombre}!</h5>
+        </div>`
+        links.innerHTML += `<li class="link" id="admin"><a class="item" href="pagAdmin.html">Panel Administración</a></li>`
     } else {
-        users = JSON.parse(localStorage.getItem('users'));
+        renderUser.innerHTML = `<h5 class="p-white"><i class="fas fa-user"></i>¡Bienvenido ${user.nombre}!<h5>`
     }
-    return users;
+    links.innerHTML +=`<li class="link" id="admin"><a class="item" href="fullPage.html">Cerrar sesión</a></li>`
 }
-function isAdmin() {
-    let users = getUsersFromLS();
-    users.forEach((user, index) => {
-        if (user.tipo === 'administrador') {
-            user.innerHTML = "Bienvenido: " + user.nombre;
-            user.innerHTML = `
-            <i class="fas fa-user-lock"></i>
-             `
-            links.innerHTMLs = `
-             <li class="link" id="admin"><a class="item" href="pagAdmin.html">Administrar</a></li>
-              `
-        } else {
-            user.innerHTML = "Bienvenido: " + user.nombre;
-            user.innerHTML = `
-            <i class="fas fa-user-lock"></i>
-             `
-        }
-    })
-    removeButtonsFromDOM();
-}
-
-
-function removeButtonsFromDOM() {
+function removeLinksFromDOM() {
     links.removeChild(login);
     links.removeChild(registro);
 }
 //Eliminar user de LS
-function removeUserFromLS(removedElement) {
-    let usersLS = getUsersFromLS;
-    usersLS.forEach((user, index) => {
-        if (user.lastName === removedElement) {
-            usersLS.splice(index, 1);
-        }
-    })
-    localStorage.setItem('users', JSON.stringify(usersLS));
+function removeUserFromLS(user) {
+    localStorage.setItem('user', JSON.stringify(user));
 }
